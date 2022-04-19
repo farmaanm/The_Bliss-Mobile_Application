909e5d42b293d254b75a2a707a2c2ff0e8dd5671
ShaviBro <shavinda20@outlook.com>
2022-04-19 21:57:20 +0530

    Contact_Activity

---
+++ app/src/main/java/com/example/thebliss/ContactActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thebliss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
--- app/src/main/java/com/example/thebliss/MainActivity.kt
+++ app/src/main/java/com/example/thebliss/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.thebliss
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -11,12 +14,13 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
@@ -27,16 +31,37 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
 
+            it.isChecked = true
+
             when(it.itemId){
 
-                R.id.nav_home -> Toast.makeText(applicationContext,"Clicked Home", Toast.LENGTH_SHORT).show()
+                /*R.id.nav_contact -> val intent = Intent(this, ContactActivity::class.java)
+                                    startActivity(intent)*/
+
+                /*R.id.nav_contact ->Toast.makeText(applicationContext,"Clicked Home", Toast.LENGTH_SHORT).show()*/
+
+                                     /*Intent homepage = new Intent(MainActivity.this, ContactActivity.class)
+                                      startActivity(homepage)*/
+
+                R.id.nav_contact -> beginActivity(ContactActivity())
             }
             true
         }
+
+        //val contact_button = findViewById<>(nav_contact)
+
     }
 
+    private fun beginActivity(activity: Activity) {
+        val intent = Intent(this, Activity::class.java).apply{
+
+        }
+        startActivity(intent)
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (onOptionsItemSelected(item)) {
+        if (toggle.onOptionsItemSelected(item) /*== R.id.nav_contact*/) {
             return true
         }
 
